53d7712f69d4af0838c25b49eb7eb939d0195e02
alex <alexchiu11@gmail.com>
2026-01-09 18:34:28 +0000

    setup better auth

--- src/routes/api/auth/$.ts
+++ src/routes/api/auth/$.ts
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { auth } from "@/infrastructure/auth/auth";
+import { auth } from "@/lib/auth";
 
 export const Route = createFileRoute("/api/auth/$")({
 	server: {
