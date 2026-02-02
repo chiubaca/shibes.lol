560db34ac12a86cad7c22ef45e9342d528e58178
alex <alexchiu11@gmail.com>
2026-02-02 19:34:28 +0000

    cleanup

--- .content-collections/generated/index.d.ts
+++
@@ -1,7 +0,0 @@
-import configuration from "../../content-collections.ts";
-import { GetTypeByName } from "@content-collections/core";
-
-export type Legal = GetTypeByName<typeof configuration, "legal">;
-export declare const allLegals: Array<Legal>;
-
-export {};
